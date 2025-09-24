328c58f0818f5e6f94ca6a1540cfff48f2ea1257
OduUdo100 <oudoiny1@jh.edu>
2025-09-23 22:52:18 -0400

    Assignment 4

---
+++ mod4_solution/harder/SpeakHello.js
@@ -0,0 +1,18 @@
+//Name: Odudu Udo-Inyang
+//Assignment 4 Module 4
+
+(function(window) {
+
+  var helloSpeaker = {};
+  var speakWord = "Hello";
+
+  helloSpeaker.speak = function (name) {
+    console.log(speakWord + " " + name);
+  }
+
+  helloSpeaker.speakSimple = function(name) {
+    return speakWord + " " + name
+  }
+
+  window.helloSpeaker = helloSpeaker;
+})(window); 
